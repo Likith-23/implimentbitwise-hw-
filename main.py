bf351730de699f6e97745cbc5582f9edc2c9b1e9
Likith-23 <likithsairajpignos@gmail.com>
2024-10-09 20:55:40 +0200

    intial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+n1 = int(input("ENTER A RANDOM NUMBER..."))
+n2 = int(input("ENATER A 2nd RANDOM NUMBER..."))
+#and operator
+print("n1 & n2 =", n1 & n2)
+#or operator
+print("n1 | n2 =", n1 | n2)
+#xor operator
+print("n1 ^ n2 =", n1 ^ n2)
+#not operator
+print(" ~ n1", ~n1)
+print(" ~ n2", ~n2)
+#right operator
+print(" n1 >> 1", n1 >> 1)
+print(" n2 >> 1",n2 >> 1)
+#not operator
+print(" n1 << 1", n1 << 1)
+print(" n2 << 1",n1 << 1)
